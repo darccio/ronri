069c0e2e82fff13df9ef5b690288ed8dd46faec7
Dario Castañé <i@dario.im>
2015-11-22 18:58:41 +0100

    First working implementation

--- ronri.go
+++ ronri.go
@@ -2,10 +2,11 @@ package ronri
 
 import (
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
+	// "github.com/davecgh/go-spew/spew"
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"reflect"
 	"strconv"
 )
 
@@ -18,8 +19,11 @@ func Eval(expression string, context *Context) (result bool, err error) {
 	switch t := expr.(type) {
 	case *ast.BinaryExpr:
 		result, err = eval(expr, context)
+	case *ast.UnaryExpr:
+		result, err = eval(expr, context)
 	case *ast.Ident:
 		result, err = eval(expr, context)
+	// TODO case *ast.CallExpr
 	default:
 		err = fmt.Errorf("only conditional expressions are supported, got %T", t)
 	}
@@ -27,7 +31,6 @@ func Eval(expression string, context *Context) (result bool, err error) {
 }
 
 func eval(expr ast.Expr, context *Context) (result bool, err error) {
-	fmt.Println(spew.Sdump(expr))
 	switch t := expr.(type) {
 	case *ast.ParenExpr:
 		x := removeStackedParens(t.X)
@@ -46,7 +49,27 @@ func eval(expr ast.Expr, context *Context) (result bool, err error) {
 	return
 }
 
+func resolve(expr ast.Expr, context *Context) (result interface{}, err error) {
+	switch t := expr.(type) {
+	case *ast.ParenExpr:
+		x := removeStackedParens(t.X)
+		result, err = resolve(x, context)
+	case *ast.BinaryExpr:
+		result, err = resolveBinaryExpr(t, context)
+	case *ast.UnaryExpr:
+		result, err = resolveUnaryExpr(t, context)
+	case *ast.Ident:
+		result, err = resolveIdent(t, context)
+	case *ast.BasicLit:
+		result, err = resolveLiteral(t, context)
+	default:
+		err = fmt.Errorf("unexpected %T", t)
+	}
+	return
+}
+
 func removeStackedParens(expr ast.Expr) (result ast.Expr) {
+	result = expr
 	parenExpr, ok := expr.(*ast.ParenExpr)
 	for ok {
 		result = parenExpr.X
@@ -63,7 +86,7 @@ func single(value interface{}, verr error) (result bool, err error) {
 	if b, ok := value.(bool); ok {
 		result = b
 	} else {
-		err = fmt.Errorf("non-bool %s (type %T) used as if condition", b)
+		err = fmt.Errorf("non-bool (type %T) used as if condition", value)
 	}
 	return
 }
@@ -77,7 +100,8 @@ func resolveLiteral(expr *ast.BasicLit, context *Context) (result interface{}, e
 	case token.CHAR:
 		result = expr.Value[1]
 	case token.STRING:
-		result = expr.Value
+		value := expr.Value
+		result = value[1 : len(value)-1]
 	default:
 		err = fmt.Errorf("literal type %s not implemented", expr.Kind)
 	}
@@ -95,6 +119,8 @@ func resolveIdent(expr *ast.Ident, context *Context) (result interface{}, err er
 		if err != nil {
 			return
 		}
+	case "nil":
+		result = nil
 	default:
 		value, ok := context.Get(name)
 		if !ok {
@@ -118,33 +144,248 @@ func resolveUnaryExpr(expr *ast.UnaryExpr, context *Context) (result bool, err e
 }
 
 func resolveBinaryExpr(expr *ast.BinaryExpr, context *Context) (result bool, err error) {
+	switch expr.Op {
+	case token.LAND, token.LOR:
+		result, err = resolveBinaryLogicalOp(expr, context)
+	case token.EQL, token.NEQ:
+		result, err = resolveComparable(expr, context)
+	case token.LSS, token.LEQ, token.GTR, token.GEQ:
+		result, err = resolveOrdered(expr, context)
+	default:
+		err = fmt.Errorf("binary operator %s not implemented", expr.Op)
+	}
+	return
+}
+
+func resolveBinaryLogicalOp(expr *ast.BinaryExpr, context *Context) (result bool, err error) {
+	var ry bool
 	rx, err := eval(expr.X, context)
 	if err != nil {
 		return
 	}
-	ry, err := eval(expr.Y, context)
+	result = rx
+	// TODO short-circuit evaluation https://en.wikipedia.org/wiki/Short-circuit_evaluation
+	switch expr.Op {
+	case token.LAND:
+		ry, err = eval(expr.Y, context)
+		if err != nil {
+			return
+		}
+		result = rx && ry
+	case token.LOR:
+		ry, err = eval(expr.Y, context)
+		if err != nil {
+			return
+		}
+		result = rx || ry
+	default:
+		err = fmt.Errorf("binary operator %s not implemented", expr.Op)
+	}
+	return
+}
+
+func resolveComparable(expr *ast.BinaryExpr, context *Context) (result bool, err error) {
+	rx, err := resolve(expr.X, context)
 	if err != nil {
 		return
 	}
-	switch expr.Op {
-	case token.LAND:
-		result = rx && ry
-	case token.LOR:
-		result = rx || ry
-	case token.EQL:
-		result = rx == ry
-	case token.NEQ:
-		result = rx != ry
-	case token.LSS:
-		result = rx < ry
-	case token.LEQ:
-		result = rx <= ry
-	case token.GTR:
-		result = rx > ry
-	case token.GEQ:
-		result = rx <= ry
+	ry, err := resolve(expr.Y, context)
+	if err != nil {
+		return
+	}
+	rxv := reflect.ValueOf(rx)
+	ryv := reflect.ValueOf(ry)
+	if !matchTypes(rxv, ryv) {
+		err = fmt.Errorf("invalid operation: mismatched types %T and %T", rx, ry)
+		return
+	}
+	switch rx.(type) {
+	case int, int8, int16, int32, int64: // rune == int32
+		result, err = compareInts(expr.Op, rxv, ryv)
+	case uint, uint8, uint16, uint32, uint64:
+		result, err = compareUints(expr.Op, rxv, ryv)
+	case float32, float64:
+		result, err = compareFloats(expr.Op, rxv, ryv)
+	case string:
+		result, err = compareStrings(expr.Op, rx, ry)
 	default:
-		err = fmt.Errorf("binary operator %s not implemented", expr.Op)
+		result, err = compareInterfaces(expr.Op, rx, ry)
+	}
+	return
+}
+
+func resolveOrdered(expr *ast.BinaryExpr, context *Context) (result bool, err error) {
+	rx, err := resolve(expr.X, context)
+	if err != nil {
+		return
+	}
+	ry, err := resolve(expr.Y, context)
+	if err != nil {
+		return
+	}
+	rxv := reflect.ValueOf(rx)
+	ryv := reflect.ValueOf(ry)
+	if !matchTypes(rxv, ryv) {
+		err = fmt.Errorf("invalid operation: mismatched types %T and %T", rx, ry)
+		return
+	}
+	if !ordered(rxv) {
+		err = fmt.Errorf("invalid operation: operator %s not defined on %T", expr.Op, rx)
+		return
+	}
+	if !ordered(ryv) {
+		err = fmt.Errorf("invalid operation: operator %s not defined on %T", expr.Op, ry)
+		return
+	}
+	switch rx.(type) {
+	case int, int8, int16, int32, int64: // rune == int32
+		result, err = compareInts(expr.Op, rxv, ryv)
+	case uint, uint8, uint16, uint32, uint64:
+		result, err = compareUints(expr.Op, rxv, ryv)
+	case float32, float64:
+		result, err = compareFloats(expr.Op, rxv, ryv)
+	case string:
+		result, err = compareStrings(expr.Op, rx, ry)
+	default:
+		err = fmt.Errorf("invalid operation: operator %s not defined on %T", expr.Op, rx)
+	}
+	return
+}
+
+func matchTypes(x, y reflect.Value) (result bool) {
+	result = x.Type() == y.Type()
+	if result {
+		return
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			result = false
+		}
+	}()
+	result = true
+	switch x.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64: // rune == int32
+		y.Int()
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		y.Uint()
+	case reflect.Float32, reflect.Float64:
+		y.Float()
+	default:
+		result = false
+	}
+	return
+}
+
+func ordered(x reflect.Value) (result bool) {
+	switch x.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		result = true
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		result = true
+	case reflect.Float32, reflect.Float64:
+		result = true
+	case reflect.String:
+		result = true
+	}
+	return
+}
+
+func compareInts(op token.Token, x, y reflect.Value) (result bool, err error) {
+	sx := x.Int()
+	sy := y.Int()
+	switch op {
+	case token.EQL:
+		result = sx == sy
+	case token.NEQ:
+		result = sx != sy
+	case token.LSS:
+		result = sx < sy
+	case token.LEQ:
+		result = sx <= sy
+	case token.GTR:
+		result = sx > sy
+	case token.GEQ:
+		result = sx >= sy
+	default:
+		err = fmt.Errorf("invalid operation: operator %s not defined on %T", op, sx)
+	}
+	return
+}
+
+func compareUints(op token.Token, x, y reflect.Value) (result bool, err error) {
+	sx := x.Uint()
+	sy := y.Uint()
+	switch op {
+	case token.EQL:
+		result = sx == sy
+	case token.NEQ:
+		result = sx != sy
+	case token.LSS:
+		result = sx < sy
+	case token.LEQ:
+		result = sx <= sy
+	case token.GTR:
+		result = sx > sy
+	case token.GEQ:
+		result = sx >= sy
+	default:
+		err = fmt.Errorf("invalid operation: operator %s not defined on %T", op, sx)
+	}
+	return
+}
+
+func compareFloats(op token.Token, x, y reflect.Value) (result bool, err error) {
+	sx := x.Float()
+	sy := y.Float()
+	switch op {
+	case token.EQL:
+		result = sx == sy
+	case token.NEQ:
+		result = sx != sy
+	case token.LSS:
+		result = sx < sy
+	case token.LEQ:
+		result = sx <= sy
+	case token.GTR:
+		result = sx > sy
+	case token.GEQ:
+		result = sx >= sy
+	default:
+		err = fmt.Errorf("invalid operation: operator %s not defined on %T", op, sx)
+	}
+	return
+}
+
+func compareStrings(op token.Token, x, y interface{}) (result bool, err error) {
+	sx := x.(string)
+	sy := y.(string)
+	switch op {
+	case token.EQL:
+		result = sx == sy
+	case token.NEQ:
+		result = sx != sy
+	case token.LSS:
+		result = sx < sy
+	case token.LEQ:
+		result = sx <= sy
+	case token.GTR:
+		result = sx > sy
+	case token.GEQ:
+		result = sx >= sy
+	default:
+		err = fmt.Errorf("invalid operation: operator %s not defined on %T", op, sx)
+	}
+	return
+}
+
+func compareInterfaces(op token.Token, x, y interface{}) (result bool, err error) {
+	switch op {
+	case token.EQL:
+		result = x == y
+	case token.NEQ:
+		result = x != y
+	default:
+		err = fmt.Errorf("invalid operation: operator %s not defined on %T", op, x)
 	}
 	return
 }
--- ronri_test.go
+++ ronri_test.go
@@ -8,22 +8,59 @@ func TestEval(t *testing.T) {
 	tests := []struct {
 		expression string
 		expected   bool
+		mustFail   bool
 	}{
-		{`((active && logged == true) || !undefined)`, false},
-		//{"(active && logged) || undefined", false},
-		//{"active && logged", false},
-		//{"active", true},
+		{`active`, true, false},
+		{`undefined`, false, true},
+		{`kind`, false, true},
+		{`active == true`, true, false},
+		{`active == false`, false, false},
+		{`!active`, false, false},
+		{`active == !active`, false, false},
+		{`!active == false`, true, false},
+		{`!active == !false`, false, false},
+		{`!active == !true`, true, false},
+		{`_`, false, true},
+		{`_ == true`, false, true},
+		{`kind == "admin"`, true, false},
+		{`uid < 200`, false, false},
+		{`uid == "200"`, false, true},
+		{`uid == 200`, true, false},
+		{`uid <= 200`, true, false},
+		{`uid > 200`, false, false},
+		{`uid >= 200`, true, false},
+		{`active && kind == "admin"`, true, false},
+		{`active && kind == "admin" && organization == "imdario"`, true, false},
+		{`organization != "dariocc"`, true, false},
+		{`kind == "admin" || organization == "dariocc"`, true, false},
+		{`kind == "admin" || organization == "dariocc" && !active`, true, false},
+		{`kind == "admin" || (organization == "dariocc" && !active)`, true, false},
+		{`(kind == "admin" || organization == "dariocc") && !active`, false, false},
+		{`kind == "admin" || kind == "operator" && active && organization == "imdario"`, true, false},
+		{`kind == "admin" || kind == "operator" && !active || organization != "imdario"`, true, false},
+		{`kind == "admin" || kind == "operator" && !active || organization != "imdario"`, true, false},
+		{`kind == "user" || kind == "operator" && !active || organization != "imdario"`, false, false},
 	}
-	environment := NewContext()
-	environment.Set("active", true)
-	environment.Set("logged", false)
+	context := NewContext()
+	context.Set("active", true)
+	context.Set("kind", "admin")
+	context.Set("organization", "imdario")
+	context.Set("uid", 200)
+	context.Set("karma", 42.314)
 	for ix, test := range tests {
-		result, err := Eval(test.expression, environment)
-		if err != nil {
+		result, err := Eval(test.expression, context)
+		if err != nil && !test.mustFail {
 			t.Fatalf("error '%s' in expression[%d]: %s", err, ix, test.expression)
 		}
 		if result != test.expected {
 			t.Fatalf("expected '%t', got '%t' on expression[%d]: %s", test.expected, result, ix, test.expression)
+		}
+		if test.mustFail {
+			if err == nil {
+				t.Fatalf("expected error but error not returned on expression[%d]: %s", ix, test.expression)
+			} else {
+				t.Logf("expression[%d] (%s) expected error: %s", ix, test.expression, err)
+			}
 		}
 	}
 }
